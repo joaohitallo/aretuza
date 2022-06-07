51ee850ede406e20e56a90109f5660d435d75e3e
João Hitallo <joaohitallo97@gmail.com>
2022-06-07 15:11:30 -0300

    Criando component buttonVoltar

--- src/components/ButtonVoltar/index.jsx
+++ src/components/ButtonVoltar/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 
 export function ButtonVoltar() {
+  const navigation = useNavigation();
+
+  function ButtonVoltar() {
+    navigation.goBack()
+
+  }
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity >
+      <TouchableOpacity onPress={ButtonVoltar}>
         <AntDesign name="arrowleft" size={34} color="black" />
       </TouchableOpacity>
     </View>
--- src/screens/signIn/index.jsx
+++ src/screens/signIn/index.jsx
@@ -12,6 +12,7 @@ export function SignIn({ navigation }) {
   function handleLogin() {
 
     navigation.navigate('Home')
+
   }
 
   return (
